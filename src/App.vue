6125cdbe693b0a98bba7de6ef50e4cb9bd57f3a3
Liam Hanrahan <outercloudstudio@gmail.com>
2026-04-29 22:54:23 -0400

    display data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref, type Ref } from 'vue';
+import type { Update } from './libs/data';
+
+const data: Ref<Update | null> = ref(null)
+
+async function update() {
+    try {
+        const response = await fetch('http://35.222.71.40:8080/api/update')
+        const json = await response.json()
+
+        data.value = json
+
+        setTimeout(update, 20);
+    } catch{
+        setTimeout(update, 1000);
+    }
+}
+
+onMounted(() => {
+    update()
+})
+
 </script>
 
 <template>
-  <HelloWorld />
+    <p style="white-space: pre;">{{ JSON.stringify(data, null, 2) }}</p>
 </template>
