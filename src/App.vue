fe548243e25a81181e53c44cc87be90a0c4c7da3
Liam Hanrahan <outercloudstudio@gmail.com>
2026-04-30 21:20:26 -0400

    live tracking

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Map from './components/Map.vue';
+
 import { onMounted, ref, type Ref } from 'vue';
 import type { Update } from './libs/data';
 
@@ -11,18 +13,21 @@ async function update() {
 
         data.value = json
 
-        setTimeout(update, 20);
+        setTimeout(update, 1000);
     } catch{
         setTimeout(update, 1000);
     }
 }
 
 onMounted(() => {
-    update()
+    // update()
 })
 
 </script>
 
 <template>
     <p style="white-space: pre;">{{ JSON.stringify(data, null, 2) }}</p>
+    <div style="width: 800px; height: 600px">
+        <Map :gps="data ? { latitude: data.latitude / 100000, longitude: data.longitude / 100000 } : null" />
+    </div>
 </template>
