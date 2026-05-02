e639c3d1233297541c74eaeffa0abaa2828d9e55
Liam Hanrahan <outercloudstudio@gmail.com>
2026-05-02 08:15:24 -0400

    ssl
    80
    
    no ssl
    
    ssl
    
    no ssl
    
    ssl

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  preview: {
-    allowedHosts: true
+    plugins: [vue()],
+    preview: {
+        allowedHosts: true,
+        // https: {
+        //     key: '/etc/letsencrypt/live/leonird.liamh.space/privkey.pem',
+        //     cert: '/etc/letsencrypt/live/leonird.liamh.space/fullchain.pem',
+        // },
+    },
+    server: {
+        host: true
     }
 })
