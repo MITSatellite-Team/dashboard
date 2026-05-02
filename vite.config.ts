38ef04553214bab9f2f51cab18402ac6837ed6d1
Liam Hanrahan <outercloudstudio@gmail.com>
2026-05-02 08:33:10 -0400

    Revert "ssl"
    This reverts commit e639c3d1233297541c74eaeffa0abaa2828d9e55.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    preview: {
-        allowedHosts: true,
-        // https: {
-        //     key: '/etc/letsencrypt/live/leonird.liamh.space/privkey.pem',
-        //     cert: '/etc/letsencrypt/live/leonird.liamh.space/fullchain.pem',
-        // },
-    },
-    server: {
-        host: true
+  plugins: [vue()],
+  preview: {
+    allowedHosts: true
     }
 })
